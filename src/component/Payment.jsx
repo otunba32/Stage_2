4db0a703127f5e6ca6809ced929ac9c16cd66254
Emmanuel Abia <emmanuel.abia32@gmail.com>
2024-07-09 22:23:09 +0100

    updated tablet screen

--- src/component/Payment.jsx
+++ src/component/Payment.jsx
@@ -12,12 +12,12 @@ const Payment = () => {
   return (
     <div>
       <h2 className=' flex justify-center font-semibold mt-5 md:font-bold md:text-[1.8rem] lg:font-bold lg:text-[2rem]'>Select Payment Method</h2>
-      <div className=" flex flex-col justify-center mt-5 pt-3 items-center gap-[6rem] md:flex-row md:mt-7 lg:m-[3rem] lg:flex lg:flex-row">
-        <button className='flex justify-center items-center bg-[#8ca1b3] border-none lg:font-bold hover:bg-green-400 text-black py-3 px-3 md:font-bold md:px-[2rem] md:py-[0.6rem] lg:px-[2rem] lg:py-[0.5rem] rounded-md gap-4'><TbArrowsExchange2  className=' hover:bg-green-400 h-[1.8rem] w-[2rem] text-gray-500  lg:h-[2rem] lg:w-[2rem] '/>
+      <div className=" flex justify-center mt-5 pt-3 font-semibold text-[0.6rem] items-center gap-2 lg:gap-[6rem] md:flex-row md:mt-7 lg:m-[3rem] lg:flex lg:flex-row">
+        <button className='flex justify-center items-center bg-[#8ca1b3] border-none gap-1 font-semibold lg:font-bold hover:bg-green-400 text-black py-3 px-1 md:text-xl md:font-bold md:px-[2rem] md:py-[0.6rem] lg:px-[2rem] lg:py-[0.5rem] rounded-md lg:gap-4'><TbArrowsExchange2  className=' hover:bg-green-400 h-[1.5rem] w-[2rem] text-gray-500  lg:h-[2rem] lg:w-[2rem] '/>
         Bank Transfer</button>
-        <button className='flex justify-center items-center text-[#FFFFFF] border-none py-3 px-3 md:px-[2rem] md:py-[0.6rem] md:font-bold lg:font-bold hover:bg-green-400 bg-[#163B5C] lg:px-[3rem] lg:py-[0.9rem] rounded-md gap-4'><CiCreditCard1 className=' h-[1.8rem] w-[2rem] text-[#FFC100] lg:h-[2rem] lg:w-[2rem] ' />
+        <button className='flex justify-center items-center text-[#FFFFFF] border-none py-3 px-3 font-semibold gap-1 md:text-xl md:px-[2rem] md:py-[0.6rem] md:font-bold lg:font-bold hover:bg-green-400 bg-[#163B5C] lg:px-[3rem] lg:py-[0.9rem] rounded-md lg:gap-4'><CiCreditCard1 className=' h-[1.8rem] w-[2rem] text-[#FFC100] lg:h-[2rem] lg:w-[2rem] ' />
         Debit Card</button>
-        <button className='flex justify-center items-center bg-[#8ca1b3] border-none font-semibold py-3 px-3 md:px-[2rem] md:py-[0.6rem] md:font-bold lg:px-[4rem] lg:font-bold lg:py-[0.9rem] hover:bg-green-400 rounded-md gap-4'><FaPaypal className=' text-gray-500 h-[1.8rem] w-[2rem] lg:h-[1.4rem] lg:w-[1.5rem] ' />
+        <button className='flex justify-center items-center bg-[#8ca1b3] border-none font-semibold py-3 px-3 md:text-xl gap-1 md:px-[2rem] md:py-[0.6rem] md:font-bold lg:px-[4rem] lg:font-bold lg:py-[0.9rem] hover:bg-green-400 rounded-md lg:gap-4'><FaPaypal className=' text-gray-500 h-[1.8rem] w-[2rem] lg:h-[1.4rem] lg:w-[1.5rem] ' />
         Paypal</button>
       </div>
       <form className=" flex flex-col mt-3 justify-center items-center md:mt-8 lg:flex ">
