f66a5172454c9b0c3f18843039984bcf4ae6d193
Emmanuel Abia <emmanuel.abia32@gmail.com>
2024-07-13 00:47:54 +0100

    product page updated

--- src/pages/productPage.jsx
+++ src/pages/productPage.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import { Footer } from '../component/Footer'
-import ProductDetail from '../component/ProductDetail';
+import React from "react";
+import { Footer } from "../component/Footer";
+import ProductDetail from "../component/ProductDetail";
+import Shop from "../component/Shop";
 
 export const Product = (prop) => {
   return (
     <div>
-      <ProductDetail />
-      
+      <Shop />
+
       <Footer />
     </div>
   );
-}
+};
