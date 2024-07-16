065c8e9f3abe4216ba0c0c7a53fe5138fa8b67f1
Emmanuel Abia <emmanuel.abia32@gmail.com>
2024-07-16 18:02:17 +0100

    Updated changes

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export const Button = ({ label, className, icon, imgStyle }) => {
+export const Button = ({ label, className, icon, imgStyle, onClick}) => {
   return (
     <div>
-        <button className={className}>
+        <button className={className} onClick={onClick}>
             <p className=' pr-[10px]'>{label}</p>
             <img src={icon} className={imgStyle} />
         </button>
